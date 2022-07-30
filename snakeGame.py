005417b5dc46f151f3848e8256413f96c32ed615
ATIII <ahtashamahsan988@gmail.com>
2022-07-30 23:24:16 +0500

    moving snake and setting game fps

--- snakeGame.py
+++ snakeGame.py
@@ -19,15 +19,22 @@ gameOver = False
 snakeX = 45
 snakeY = 55
 snakeSize = 10
+fps = 30            #frames per second
 
+
+clock = pygame.time.Clock()
 
 while not exitGame:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exitGame = TRUE
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                snakeX = snakeX+10
     gameWindow.fill(white)
     pygame.draw.rect(gameWindow, black, [snakeX, snakeY, snakeSize, snakeSize])
     pygame.display.update()
+    clock.tick(fps)
 
 pygame.quit()
 quit()
