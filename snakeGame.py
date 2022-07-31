c7ac1e67d196c02d06cd71fd515ec802d35632d0
ATIII <ahtashamahsan988@gmail.com>
2022-07-31 14:44:21 +0500

    incremented the snake size on eating

--- snakeGame.py
+++ snakeGame.py
@@ -1,4 +1,5 @@
 import random
+from turtle import color
 import pygame
 pygame.init()
 
@@ -35,38 +36,52 @@ def screenScore(text, color, x, y):
     
     # blit is used to update it all on screen
     gameWindow.blit(screenText, [x, y]) 
+
+snakeList = []
+snakeLength = 1
+
+def plotSnake(gameWindow, color, snakeList, snakeSize):
+    for x, y in snakeList:
+        pygame.draw.rect(gameWindow, color, [x, y, snakeSize, snakeSize])
 while not exitGame:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exitGame = True
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
-                velocityX = 10
+                velocityX = 5
                 velocityY = 0
             if event.key == pygame.K_LEFT:
                 velocityY = 0
-                velocityX = -10
+                velocityX = -5
             if event.key == pygame.K_UP:
                 velocityX = 0
-                velocityY = -10
+                velocityY = -5
             if event.key == pygame.K_DOWN:
                 velocityX = 0
-                velocityY = 10
+                velocityY = 5
     
     snakeX = snakeX + velocityX
     snakeY = snakeY + velocityY
     if abs(snakeX-foodX)<6 and abs(snakeY-foodY)<6:
         score += 1
-        
         foodX = random.randint(20, screenWidth/2)
         foodY = random.randint(20, screenHeight/2) 
-        print('Score: ', score)
-
+        snakeLength += 5
+        
 
     gameWindow.fill(white)
     screenScore('Score: ' + str(score), red, 5, 5)
-    pygame.draw.rect(gameWindow, black, [snakeX, snakeY, snakeSize, snakeSize])
+    # pygame.draw.rect(gameWindow, black, [snakeX, snakeY, snakeSize, snakeSize])
     pygame.draw.rect(gameWindow, red, [foodX, foodY, snakeSize, snakeSize])
+
+    head = []
+    head.append(snakeX)
+    head.append(snakeY)
+    snakeList.append(head)
+    if len(snakeList) >snakeLength:
+        del snakeList[0]
+    plotSnake(gameWindow, black, snakeList, snakeSize)
     pygame.display.update()
     clock.tick(fps)
 
